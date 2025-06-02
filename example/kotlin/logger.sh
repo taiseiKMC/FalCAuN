4d6b9c2a6f4d9931b18b391db830b0a73d38998b
taiseiKMC <taisei@kmc.gr.jp>
2025-06-02 23:19:33 +0900

    Fixup logger
    Process ID は意味ない... killall java

--- example/kotlin/logger.sh
+++ example/kotlin/logger.sh
@@ -8,8 +8,7 @@ DATE=$(date '+%y%m%d%H%M')
 FILENAME="${NAME%.*}-${DATE}.log"
 
 echo "Log File:  ${FOLDER}/${FILENAME}" 
-kscript $1 2>&1 > "${FOLDER}/${FILENAME}" &
+nohup kscript $1 > "${FOLDER}/${FILENAME}" 2>&1 &
 echo "Process: $!"
-disown %1
 
 
