84168d08c0bd694219d361067f612fd75ebfc114
taiseiKMC <taisei@kmc.gr.jp>
2025-06-12 02:35:47 +0900

    Fix simglucode.kts : Use PythonNumericSUL

--- example/kotlin/simglucose.kts
+++ example/kotlin/simglucose.kts
@@ -78,7 +78,7 @@ val crossoverProb = 0.5
 val mutationProb = 0.01
 
 // Load the simglucose model implemented by python
-PythonContinuousNumericSUL(initScript, signalStep).use { autoTransSUL ->
+PythonNumericSUL(initScript, signalStep).use { autoTransSUL ->
     // Configure and run the verifier
     val verifier = NumericSULVerifier(autoTransSUL, signalStep, properties, mapper)
     // Timeout must be set before adding equivalence testing
